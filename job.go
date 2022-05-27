9d7cdba64d9f7d53bc4b1345af10b6c4ab20848a
Albertwzp <albert.wzp@gmail.com>
2022-05-27 11:24:37 +0800

    debug GetBuild

--- job.go
+++ job.go
@@ -126,8 +126,11 @@ func (j *Job) GetBuild(ctx context.Context, id int64) (*Build, error) {
 	// i.e. Server : https://<domain>/jenkins/job/JOB1
 	// "https://<domain>/jenkins/" is the server URL,
 	// we are expecting jobURL = "job/JOB1"
-	jobURL := strings.Replace(j.Raw.URL, j.Jenkins.Server, "", -1)
+	fmt.Printf("j.Raw.URL: %s, j.Jenkins.Server: %s\n", j.Raw.URL, j.Jenkins.Server)
+	//jobURL := strings.Replace(j.Raw.URL, j.Jenkins.Server, "", -1)
+	jobURL := url.Parse(j.Raw.URL).Path
 	build := Build{Jenkins: j.Jenkins, Job: j, Raw: new(BuildResponse), Depth: 1, Base: jobURL + "/" + strconv.FormatInt(id, 10)}
+	fmt.Print(build)
 	status, err := build.Poll(ctx)
 	if err != nil {
 		return nil, err
